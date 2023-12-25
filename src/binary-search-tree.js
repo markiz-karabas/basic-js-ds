cb1a4e39aad3b9b89e69efed37e90f6e2a08b2fa
markiz-karabas <vsevolodova@inbox.ru>
2023-12-26 01:37:51 +0300

    feat: 11 failing, 9 passing

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,41 +1,111 @@
 const { NotImplementedError } = require('../extensions/index.js');
-
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
+
 class BinarySearchTree {
+  constructor() {
+    this.rootNode = null;
+  }
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.rootNode;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    const newNode = new Node(data);
+    if (!this.root) {
+      this.root = newNode;
+      return;
+    }
+    let currentNode = this.root;
+    while (currentNode) {
+      if(newNode.data < currentNode.data) {
+        if (!currentNode.left) {
+          currentNode.left = newNode;
+          return;
+        }
+        currentNode = currentNode.left;
+      } else {
+        if (!currentNode.right) {
+          currentNode.right = newNode;
+          return;
+        }
+        currentNode = currentNode.right;
+      }
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    const newNode = new Node(data);
+    let currentNode = this.root;
+    while (newNode) {
+      if (data === newNode.data) {
+        return true;
+      }
+      if (data < newNode.data) {
+        newNode = newNode.left;
+      } else {
+        newNode = newNode.right;
+      }
+    }
+    return false;
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    let currentNode = this.root;
+    if (currentNode && currentNode.data === data) {
+      return currentNode;
+    }
+    if (currentNode.left && currentNode.data > data) {
+      return currentNode.left.find(data);
+    }
+    if (currentNode.right && currentNode.data < data) {
+      return currentNode.right.find(data);
+    }
+    return null;
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data) {
+    let currentNode = this.root;
+    if (currentNode.data && currentNode.data === data) {
+        if (!currentNode.left && !currentNode.right) {
+          this.rootNode = null;
+        } else if (!currentNode.left) {
+          this.rootNode = currentNode.right.root();
+        } else if (!currentNode.right) {
+          this.rootNode = currentNode.left.root();
+        } else {
+          const minRightNode = currentNode.right.min();
+          currentNode.right.remove(minRightNode);
+          this.rootNode.data = minRightNode;
+        }
+        return;
+      }
+    if (currentNode.data > data) {
+        currentNode.left.remove(data);
+      } else {
+        currentNode.right.remove(data);
+      }
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let currentNode = this.root;
+    if (!currentNode) {
+      return null;
+    }
+    if (!currentNode.left) {
+      return currentNode.data;
+    } else {
+      if (!currentNode.left.root()) {
+        this.rootNode.left = null;
+        return currentNode.data;
+      }
+      return currentNode.left.min();
+    }
   }
 
   max() {
--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -22,9 +22,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  let currentNode = l;
+  let prevNode = null;
+  let result;
+  while (currentNode !== null) {
+      if (currentNode.value === k) {
+          if (prevNode) {
+            prevNode.next = currentNode.next;
+          } else {
+            result = currentNode.next;
+          }
+      } else {
+        prevNode = currentNode;
+      }
+    currentNode = currentNode.next;
+  }
+  return result;
 }
 
 module.exports = {
